44f323364a4e807142928b13468e82b7e55b3613
Codi-p <370015851@qq.com>
2019-11-10 16:08:17 +0800

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+# !python
+#
+
+from flask import Flask
+from flask import current_app
+from flask import g
+from flask import request
+from flask import session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():    
+    return '<h1>Hello world!aaa your browser is {} </h1>'.format(request.headers)
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {}!</h1>'.format(name)
+
